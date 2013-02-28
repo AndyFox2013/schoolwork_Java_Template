cd27cd0ce21914440c7b3ac310fe2d5a955f72ce
Andrew Montgomery-Fox <andrewmf2011@gmail.com>
2013-02-28 16:48:44 +0000

    First commit

---
+++ MyProg.java
@@ -0,0 +1,28 @@
+
+/**
+ * Program Title.
+ * Description of what program does ...
+ * ...
+ * 
+ * @author (Your name here...) 
+ * @version (Date or version number)
+ */
+
+import javax.swing.*;
+import java.text.*;
+import java.util.*;
+
+public class MyProg 
+{
+    
+    
+     //Main method - required by all programs
+   
+    public static void main(String[] args)
+    {
+        // Type code from here...
+        
+    }
+    
+    
+}
